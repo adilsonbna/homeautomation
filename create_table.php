8e2f006a60a688dbfb48abeb368c55b45360e05c
adilsonbna <adilsonbna@gmail.com>
2016-09-01 14:40:04 -0300

    Update create_table.php

--- create_table.php
+++ create_table.php
@@ -10,7 +10,7 @@
 	$conn = mysql_connect(DBHOST,DBUSER,DBPASS);
 	$dbcon = mysql_select_db(DBNAME);
 
-	mysql_query = (CREATE TABLE IF NOT EXISTS `users` (`userId` int(11) NOT NULL AUTO_INCREMENT, `userName` varchar(30) NOT NULL, `userEmail` varchar(60) NOT NULL, `userPass` varchar(255) NOT NULL, PRIMARY KEY (`userId`), UNIQUE KEY `userEmail` (`userEmail`) ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1);
+	mysql_query = (CREATE TABLE IF NOT EXISTS `users` (`userId` int(11) NOT NULL AUTO_INCREMENT, `userName` varchar(30) NOT NULL, `userEmail` varchar(60) NOT NULL, `userPass` varchar(255) NOT NULL, PRIMARY KEY (`userId`), UNIQUE KEY `userEmail` (`userEmail`));
 	
 	if ( !$conn ) {
 		die("Connection failed : " . mysql_error());
