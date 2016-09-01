ec8d5bf67a83219baa75efd2d8a27ac983a9c270
adilsonbna <adilsonbna@gmail.com>
2016-09-01 11:56:40 -0300

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -2,10 +2,10 @@
 
 	error_reporting( ~E_ALL & ~E_DEPRECATED &  ~E_NOTICE );
 	
-	define('DBHOST', 'fdb15.mutanthost.com');
-	define('DBUSER', '2186856_db');
-	define('DBPASS', 'HM3NuCrSBx');
-	define('DBNAME', '2186856_db');
+	define('DBHOST', 'iot.bounceme.net');
+	define('DBUSER', 'root');
+	define('DBPASS', 'deadfish22');
+	define('DBNAME', 'iot');
 	
 	$conn = mysql_connect(DBHOST,DBUSER,DBPASS);
 	$dbcon = mysql_select_db(DBNAME);
