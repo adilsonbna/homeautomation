8eedfa05694a18ef506fee3d1503c8e870e18ff7
adilsonbna <adilsonbna@gmail.com>
2016-09-01 13:43:51 -0300

    Update create_table.php

--- create_table.php
+++ create_table.php
@@ -5,7 +5,7 @@ $password = "deadfish22";
 $dbname = "iot";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysql($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
