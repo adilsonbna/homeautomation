2e32a00096d33647693aeba4d1792461965291b6
adilsonbna <adilsonbna@gmail.com>
2016-09-01 14:15:00 -0300

    Update create_table.php

--- create_table.php
+++ create_table.php
@@ -13,8 +13,8 @@
 	$sql = "CREATE TABLE MyGuests (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, firstname VARCHAR(30) NOT NULL, lastname VARCHAR(30) NOT NULL,
 email VARCHAR(50), reg_date TIMESTAMP)";
 	
-	mysql_query=("CREATE TABLE MyGuests (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, firstname VARCHAR(30) NOT NULL, lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50), reg_date TIMESTAMP");
+	mysql_query = (CREATE TABLE IF NOT EXISTS `users` (`userId` int(11) NOT NULL AUTO_INCREMENT, `userName` varchar(30) NOT NULL, `userEmail` varchar(60) NOT NULL,
+  `userPass` varchar(255) NOT NULL, PRIMARY KEY (`userId`), UNIQUE KEY `userEmail` (`userEmail`) ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1);
 	
 	if ( !$conn ) {
 		die("Connection failed : " . mysql_error());
