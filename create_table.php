26d8207658000846e73e088cb4c75b3637c12b00
adilsonbna <adilsonbna@gmail.com>
2016-09-01 14:00:47 -0300

    Update create_table.php

--- create_table.php
+++ create_table.php
@@ -1,32 +1,31 @@
 <?php
-$servername = "iot";
-$username = "iot";
-$password = "deadfish22";
-$dbname = "iot";
 
-// Create connection
-$conn = new mysql($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-// sql to create table
-$sql = "CREATE TABLE IF NOT EXISTS `users` (
-  `userId` int(11) NOT NULL AUTO_INCREMENT,
-  `userName` varchar(30) NOT NULL,
-  `userEmail` varchar(60) NOT NULL,
-  `userPass` varchar(255) NOT NULL,
-  PRIMARY KEY (`userId`),
-  UNIQUE KEY `userEmail` (`userEmail`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+	error_reporting( ~E_ALL & ~E_DEPRECATED &  ~E_NOTICE );
+	
+	define('DBHOST', 'iot');
+	define('DBUSER', 'iot');
+	define('DBPASS', 'deadfish2');
+	define('DBNAME', 'iot');
+	
+	$conn = mysql_connect(DBHOST,DBUSER,DBPASS);
+	$dbcon = mysql_select_db(DBNAME);
+	
+	$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP
 )";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Table MyGuests created successfully";
-} else {
-    echo "Error creating table: " . $conn->error;
-}
-
-$conn->close();
-?>
+	
+	mysql_query=($sql);
+	
+	if ( !$conn ) {
+		die("Connection failed : " . mysql_error());
+	}
+	
+	if ( !$dbcon ) {
+		die("Database Connection failed : " . mysql_error());
+	}
+	
+	?>
